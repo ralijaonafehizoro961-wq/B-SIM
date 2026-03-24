2d91afb86277ad74e586f000412854ee58f071c2
kalianah <kalianahkali@gmail.com>
2026-03-24 19:58:50 +0100

    without del and new

--- src/main/java/model/TypeCompte.java
+++ src/main/java/model/TypeCompte.java
@@ -3,26 +3,26 @@ package model;
 public class TypeCompte {
 	
 	protected String Libelle_Cpt;
-	protected String Type_Ctp;
-	public double Taux;
+	protected String Type_Cpt;
+	protected double Taux;
 	protected double Dec_Max_A;
 	protected double Plafond_Cte;
 	
-	public Type_Cte(String libellecpt, String typecpt, double taux, double dec_max_a, double plafond_cte) {
+	public TypeCompte(String libellecpt, String typecpt, double taux, double dec_max_a, double plafond_cte) {
 		this.Libelle_Cpt = libellecpt;
-		this.Type_Ctp = typecpt;
+		this.Type_Cpt = typecpt;
 		this.Taux = taux;
 		this.Dec_Max_A = dec_max_a;
 		this.Plafond_Cte = plafond_cte;
 	}
 	
-	public Modifier_Taux(double n_taux) {
+	public void Modifier_Taux(double n_taux) {
 		this.Taux = n_taux;
 	}
 	
 	//need DBB here
 	
-	public Afficher_Type() {
+	public String Afficher_Type() {
 		return this.Type_Cpt;
 	}
 	
