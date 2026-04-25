0e11369fc3f7b2cde9c58ed6aa0767e5c58e3935
kalianah <kalianahkali@gmail.com>
2026-04-25 20:36:52 +0200

    Type de compte

--- src/main/java/model/TypeCompte.java
+++ src/main/java/model/TypeCompte.java
@@ -1,30 +1,54 @@
 package model;
 
 public class TypeCompte {
+
+	private String libelleCpt;
+	private String typeCpt;
+	private double taux;
+	private double decMaxA;
+	private double plafondCte;
 	
-	private String Libelle_Cpt;
-	private String Type_Cpt;
-	private double Taux;
-	private double Dec_Max_A;
-	private double Plafond_Cte;
+	public TypeCompte(String l, String t, double ta, double d, double pl) {
 	
-	public TypeCompte(String libellecpt, String typecpt, double taux, double dec_max_a, double plafond_cte) {
-		this.Libelle_Cpt = libellecpt;
-		this.Type_Cpt = typecpt;
-		this.Taux = taux;
-		this.Dec_Max_A = dec_max_a;
-		this.Plafond_Cte = plafond_cte;
+		this.libelleCpt = l;
+		this.typeCpt = t;
+		this.taux = ta;
+		this.decMaxA = d;
+		this.plafondCte = pl;
+	
 	}
 	
-	public void Modifier_Taux(double n_taux) {
-		this.Taux = n_taux;
+	public String getLibelleCpt() {
+		return this.libelleCpt;
 	}
 	
-	
-	public String Afficher_Type() {
-		return this.Type_Cpt;
+	public String getTypeCpt() {
+		return this.typeCpt;
 	}
 	
+	public double getTaux() {
+		return this.taux;
+	}
 	
+	public double getDecMaxA() {
+		return this.decMaxA;
+	}
 	
+	public double getPlafondCte() {
+		return this.plafondCte;
+	}
+	
+	public void setTaux(double new_taux) {
+		this.taux = new_taux;
+	}
+	
+	public void setDecMaxA(double new_dec) {
+		this.decMaxA = new_dec;
+	}
+	
+	public void setPlafondCte(double new_pl) {
+		this.plafondCte = new_pl;
+	}
+	
+
 }
